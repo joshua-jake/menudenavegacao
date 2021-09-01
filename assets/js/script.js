a745a59807ddd3c85fbc8e520c484b434a0074b5
Jesuan Goalhath <jesuanprincdd@gmail.com>
2021-08-31 22:23:30 -0300

    Menu de naveção na lateral utilização função javascript para que ao clicar nas barras que estão na lateral abra-se o menu

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function abrir() {
+    
+   let menuArea = document.getElementById("menu-area");
+
+   if(menuArea.classList.contains('menu-opened') == true) {
+    menuArea.classList.remove('menu-opened');    
+   } 
+   else {
+       menuArea.classList.add('menu-opened');
+   }
+
+} 
